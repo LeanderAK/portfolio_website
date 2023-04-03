7f4c1994ee55c059be61514482550cd2cd97ec82
Leander Kammermeier <leander.kammermeier@jkaref.de>
2023-04-03 17:10:21 +0200

    added readme

--- src/App.js
+++ src/App.js
@@ -4,22 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-          BLABLABLA
-          
-        </a>
-      </header>
+      <h1> Leander Kammermeier </h1>
+
+
+      
     </div>
   );
 }
